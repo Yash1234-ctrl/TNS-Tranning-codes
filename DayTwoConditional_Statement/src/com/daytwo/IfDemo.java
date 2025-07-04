8b020c67dd1a75567f75a7ab165a156abe1af5cd
Yash1234-ctrl <yashimamdar@gmail.com>
2025-07-04 22:09:15 +0530

    Conditional Statement

---
+++ DayTwoConditional_Statement/src/com/daytwo/IfDemo.java
@@ -0,0 +1,15 @@
+package com.daytwo;
+
+public class IfDemo {
+	public static void main(String[] args)
+	{
+		int a= 100;
+		int b = 20;
+		
+		if(b>a)
+		{
+		    System.out.println("B is greater number");	
+		}
+	}
+
+}
