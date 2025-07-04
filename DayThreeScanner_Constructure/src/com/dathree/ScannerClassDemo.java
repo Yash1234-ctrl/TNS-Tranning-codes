3cc3e630fed776729782c2b84ba7a510c8e96cff
Yash1234-ctrl <yashimamdar@gmail.com>
2025-07-04 22:09:59 +0530

    Day three

---
+++ DayThreeScanner_Constructure/src/com/dathree/ScannerClassDemo.java
@@ -0,0 +1,19 @@
+package com.dathree;
+import java.util.Scanner;
+
+public class ScannerClassDemo {
+	public static void main(String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter your ID : ");
+		int id = sc.nextInt();
+		System.out.println("ID = "+id);
+		
+		
+		System.out.println("Enter your name :");
+		String name= sc.next();
+		System.out.println("Name = "+ name);
+		
+	}
+
+}
