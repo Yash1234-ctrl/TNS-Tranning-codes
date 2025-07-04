4e0b354c1fe0e3ce7cb812071bc5444606dc4e38
Yash1234-ctrl <yashimamdar@gmail.com>
2025-07-04 22:08:20 +0530

    Day two Looping Statement

---
+++ DayTwoLoopingStatement/src/com/DoWhileLoopDemo.java
@@ -0,0 +1,15 @@
+package com;
+
+public class DoWhileLoopDemo {
+
+	public static void main(String[] args) {
+
+		int i =10;
+		do {
+			System.out.println(i);
+			i++;
+		}
+		while(i<=1);
+	}
+
+}
---
+++ DayTwoLoopingStatement/src/com/ForLoopDemo.java
@@ -0,0 +1,16 @@
+package com;
+
+public class ForLoopDemo {
+public static void main(String[] args) {
+		
+		for(int i=100; i>=1; --i)
+		{
+			System.out.println("Value of i : "+ i);
+		}
+
+		
+			
+		
+	}
+
+}
