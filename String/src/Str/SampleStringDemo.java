91e51355c562871865081d05c75e43f6757787fe
Yash1234-ctrl <yashimamdar@gmail.com>
2025-11-21 13:00:15 +0530

    Add files via upload
    String in Java

---
+++ String/src/Str/SampleStringDemo.java
@@ -0,0 +1,16 @@
+package Str;
+
+public class SampleStringDemo {
+	public static void main(String[] args) {
+
+		String s1 ="Hello World";  // literal method
+		System.out.println(s1);
+		
+		String s2 = new String("String Methods");// object type 
+		System.out.println(s2);
+		
+		String longstr= "This is how "+ "long string "+"we stores";
+		System.out.println(longstr);
+	}
+
+}
